c9108d3747aea0df1e03cf66bd1e948d572671ae
Abishek_1409 <abisheklive09@gmail.com>
2026-08-25 14:23:21 +0530

    Include Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Local-only Firebase configuration. This file is ignored by Git.
+window.firebaseConfig = {
+  apiKey: "AIzaSyB0nwbFYOOrP6oa2Q63EgQJLHB2BY_3MOE",
+  authDomain: "bfs-qr-ordering.firebaseapp.com",
+  databaseURL: "https://bfs-qr-ordering-default-rtdb.firebaseio.com",
+  projectId: "bfs-qr-ordering",
+  storageBucket: "bfs-qr-ordering.firebasestorage.app",
+  messagingSenderId: "467780773395",
+  appId: "1:467780773395:web:34a243a8f672a74249c1e6",
+  measurementId: "G-WV9FMCEKMB"
+};
