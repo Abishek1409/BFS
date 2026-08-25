4a2a8401de3de53594b1981e138ec542ebe02fed
Abishek_1409 <abisheklive09@gmail.com>
2026-08-25 13:40:07 +0530

    Firebase added

---
+++ firebase-config.example.js
@@ -0,0 +1,11 @@
+// Copy this file to firebase-config.js and replace the placeholders.
+window.firebaseConfig = {
+  apiKey: "YOUR_FIREBASE_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
