bbb08b509f7253b0a0f388a81799e89610c120d3
Abishek_1409 <abisheklive09@gmail.com>
2026-09-16 13:45:55 +0530

    Fix Firebase config and favicon deployment

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase web configuration. These values are public client settings.
+window.firebaseConfig = {
+  apiKey: "AIzaSyB0nwbFYOOrP6oa2Q63EgQJLHB2BY_3MOE",
+  authDomain: "bfs-qr-ordering.firebaseapp.com",
+  databaseURL: "https://bfs-qr-ordering-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bfs-qr-ordering",
+  storageBucket: "bfs-qr-ordering.firebasestorage.app",
+  messagingSenderId: "467780773395",
+  appId: "1:467780773395:web:34a243a8f672a74249c1e6",
+  measurementId: "G-WV9FMCEKMB"
+};
